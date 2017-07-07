33a163a54aae9bae475bb88f76b1b4d04852799a
Wouter Saaltink <wouter.saaltink@gmail.com>
2017-07-07 20:56:39 +0200

    Move texture loader outside texture header and into new loader header

--- include/gl-utilities-textures.h
+++ include/gl-utilities-textures.h
@@ -7,6 +7,7 @@
 
 class Texture
 {
+    friend class TextureLoader;
     GLuint _textureId;
     int _width;
     int _height;
@@ -32,32 +33,7 @@ public:
             this->_textureId = 0;
         }
     }
-// When STB image is included, we can load images trough this library
-#ifdef STBI_INCLUDE_STB_IMAGE_H
-    bool load(const std::string& filename)
-    {
-        int x = 0, y = 0, comp = 3;
-        auto imageData = stbi_load(filename.c_str(), &x, &y, &comp, 4);
-        if (imageData != nullptr)
-        {
-            this->_width = x;
-            this->_height = y;
-            std::cout << "loaded " << filename << std::endl;
-            auto format = comp == 4 ? GL_RGBA : GL_RGB;
-            glBindTexture(GL_TEXTURE_2D, this->_textureId);
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-            glTexImage2D(GL_TEXTURE_2D, 0, format, x, y, 0, format, GL_UNSIGNED_BYTE, imageData);
-            free(imageData);
-
-            return true;
-        }
-
-        std::cout << "Unable to load " << filename << std::endl;
-        return false;
-    }
-#endif // STBI_INCLUDE_STB_IMAGE_H
-
+    
     int width() const { return this->_width; }
     int height() const { return this->_height; }
 };
